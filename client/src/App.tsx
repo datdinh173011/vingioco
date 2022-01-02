3627d537e49290ecc04715a559029eabef767500
Dang Minh Nguyen <dangnm.cs@gmail.com>
2022-01-02 20:43:49 +0700

    Add 1st version of pomodoro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import LoginDialog from './components/LoginDialog'
 import ComputerDialog from './components/ComputerDialog'
 import VideoConnectionDialog from './components/VideoConnectionDialog'
 import Chat from './components/Chat'
+import Pomodoro from './components/Pomodoro'
 // import Debug from './components/Debug'
 
 function App() {
@@ -13,12 +14,14 @@ function App() {
   const computerDialogOpen = useAppSelector((state) => state.computer.computerDialogOpen)
   const videoConnected = useAppSelector((state) => state.user.videoConnected)
 
+  console.log("loggedIn ", loggedIn);
   return (
     <div className="App">
       {/* <Debug /> */}
 
       {/* Render the LoginDialog if not logged in, else render Chat. */}
       {loggedIn ? <Chat /> : <LoginDialog />}
+      {loggedIn && <Pomodoro />}
 
       {/* Render the ComputerDialog if user is using a computer. */}
       {computerDialogOpen && <ComputerDialog />}
